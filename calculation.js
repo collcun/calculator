00bdc22c0a2bbc626e36690762d1c387572be97f
collicun <collicun@gmail.com>
2018-01-04 23:01:50 -0600

    add excitement

--- calculation.js
+++ calculation.js
@@ -1,4 +1,4 @@
-console.log("Now calculating the sum of 2 and 2");
+console.log("Now calculating the sum of 2 and 2!");
 console.log(2 + 2);
-console.log("Now calculating the product of 2 and 2");
+console.log("Now calculating the product of 2 and 2!");
 console.log(2 * 2);
