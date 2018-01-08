3d7259e313b55f54a541dba0e616796b24a30e1d
collicun <collicun@gmail.com>
2018-01-07 22:12:12 -0600

    be more polite

--- calculation.js
+++ calculation.js
@@ -1,4 +1,6 @@
+console.log("Hello.")
 console.log("Now calculating the sum and product of 2 and 2!");
 console.log(2 + 2);
 console.log("Now calculating the sum of 3 and 2!");
 console.log(3 + 2);
+console.log("Goodbye.")
