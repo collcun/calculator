4f539a2984ea8f6681014650d2b5b48551c2d40b
Collin Cunninghame <collicun@gmail.com>
2018-01-08 17:18:55 -0600

    move from console.log to dom manipulation

--- calculation.js
+++ calculation.js
@@ -1,6 +1,1 @@
-console.log("Hello.");
-console.log("Now calculating the sum and product of 2 and 2!");
-console.log(2 + 2);
-console.log("Now calculating the sum of 3 and 2!");
-console.log(3 + 2);
-console.log("Goodbye.");
+document.getElementById("calculation").innerHTML=String(2+2);
