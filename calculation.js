ed60541e2796cb881c477177271ee07629f5e09b
Collin Cunninghame <collicun@gmail.com>
2018-01-28 13:00:45 -0600

    let user also choose product operation

--- calculation.js
+++ calculation.js
@@ -1,4 +1,17 @@
+var answer = 0;
+var values = parseInt($("select#first").val()) + parseInt($("select#second").val());
+var operation = $("select#function").val();
+
 function calculate() {
-  let answer = parseInt($("select#first").val()) + parseInt($("select#second").val());
+  values = [parseInt($("select#first").val()), parseInt($("select#second").val())];
+  operation = $("select#function").val();
+  switch (operation) {
+    case "sum":
+      answer = values[0] + values[1];
+      break;
+    case "product":
+      answer = values[0] * values[1];
+      break;
+  }
   document.getElementById("calculation").innerHTML=String(answer);
 }
